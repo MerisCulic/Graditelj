514739a3fbb1cf590453fd9f4be2eab64091f1f7
Meris Čulić <meris.culic@gmail.com>
2022-02-07 20:57:48 +0100

    Add slides to second section

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,12 @@ const FullPageConfig = new fullpage('#fullpage', {
     licenseKey: null,
     sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#ccddff'],
     anchors: ['Graditelj', 'Storitve', 'thirdPage', 'fourthPage'],
-    menu: '#fullpage',
     css3: true,
-    scrollingSpeed: 700
+    scrollingSpeed: 700,
+    slidesNavigation: true,
+	slidesNavPosition: 'bottom',
+    controlArrows: false,
+    keyboardScrolling: true
 });
 
 const scene = document.getElementById('parallax-scene');
